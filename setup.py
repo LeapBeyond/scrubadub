2079963594f335af2a0406fa3ab0fd16ebdbf511
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2015-04-05 15:02:40 -0500

    porting setup.py schtuff

---
+++ setup.py
@@ -0,0 +1,41 @@
+import glob
+import os
+from setuptools import setup
+
+import scrubadub
+
+# get all of the scripts
+scripts = glob.glob("bin/*")
+
+# read in the description from README
+with open("README.md") as stream:
+    long_description = stream.read()
+
+github_url='https://github.com/deanmalmgren/scrubadub'
+
+# read in the dependencies from the virtualenv requirements file
+dependencies = []
+filename = os.path.join("requirements", "python")
+with open(filename, 'r') as stream:
+    for line in stream:
+        package = line.strip().split('#')[0]
+        if package:
+            dependencies.append(package)
+
+setup(
+    name=scrubadub.__name__,
+    version=scrubadub.VERSION,
+    description="",
+    long_description=long_description,
+    url=github_url,
+    download_url="%s/archives/master" % github_url,
+    author='Dean Malmgren',
+    author_email='dean.malmgren@datascopeanalytics.com',
+    license='MIT',
+    scripts=scripts,
+    packages=[
+        'scrubadub',
+    ],
+    install_requires=dependencies,
+    zip_safe=False,
+)
