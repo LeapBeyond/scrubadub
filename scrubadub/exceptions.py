9e4c5b355b11cbcba5a8c440803005ce3cd88569
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2015-04-08 13:29:47 -0500

    tests passing

--- scrubadub/exceptions.py
+++ scrubadub/exceptions.py
@@ -1,13 +1,18 @@
 
+
 # this is the base exception that is thrown by scrubadub to make it
 # easy to suppress all Scrubadub exceptions
 class ScrubadubException(Exception):
+
     def render(self, msg):
         return msg % vars(self)
 
 
 class UnicodeRequired(ScrubadubException):
-    """Scrubadub requires"""
+    """Scrubadub requires unicode. Throw a useful error to lead users to
+    the promised land.
+    """
+
     def __str__(self):
         return self.render((
             'scrubadub works best with unicode.\n'
@@ -16,4 +21,3 @@ class UnicodeRequired(ScrubadubException):
             'But unicode sandwiches are awesome.\n'
             'http://bit.ly/unipain @nedbat\n'
         ))
-
--- tests/test_placeholder.py
+++ tests/test_placeholder.py
@@ -8,37 +8,37 @@ class PlaceholderTestCase(unittest.TestCase):
     def test_john(self):
         """Make sure proper names are removed from the text"""
         self.assertEqual(
-            scrubadub.clean_with_placeholders('John is a cat'),
-            '{{NAME}} is a cat',
+            scrubadub.clean_with_placeholders(u'John is a cat'),
+            u'{{NAME}} is a cat',
             'John not replaced with {{NAME}}',
         )
 
     def test_gmail_john(self):
         """Make sure email addresses are removed from text"""
         self.assertEqual(
-            scrubadub.clean_with_placeholders('My email is john@gmail.com'),
-            'My email is {{EMAIL}}',
+            scrubadub.clean_with_placeholders(u'My email is john@gmail.com'),
+            u'My email is {{EMAIL}}',
             'john@gmail.com is not replaced with {{EMAIL}}',
         )
 
     def test_fancy_gmail_john(self):
         """Make sure email addresses are removed from text"""
         self.assertEqual(
-            scrubadub.clean_with_placeholders('My email is john at gmail.com'),
-            'My email is {{EMAIL}}',
+            scrubadub.clean_with_placeholders(u'My email is john at gmail.com'),
+            u'My email is {{EMAIL}}',
             'john at gmail.com is not replaced with {{EMAIL}}',
         )
         self.assertEqual(
-            scrubadub.clean_with_placeholders('My email is john AT gmail.com'),
-            'My email is {{EMAIL}}',
+            scrubadub.clean_with_placeholders(u'My email is john AT gmail.com'),
+            u'My email is {{EMAIL}}',
             'john AT gmail.com is not replaced with {{EMAIL}}',
         )
 
     def test_empty(self):
         """Make sure this returns an empty string"""
         self.assertEqual(
-            scrubadub.clean_with_placeholders(''),
-            '',
+            scrubadub.clean_with_placeholders(u''),
+            u'',
             'empty string is not preserved',
         )
 
@@ -46,6 +46,6 @@ class PlaceholderTestCase(unittest.TestCase):
         """Make sure unicode works, too"""
         self.assertEqual(
             scrubadub.clean_with_placeholders(u'John is a cat'),
-            '{{NAME}} is a cat',
+            u'{{NAME}} is a cat',
             'unicode strings work too',
         )
