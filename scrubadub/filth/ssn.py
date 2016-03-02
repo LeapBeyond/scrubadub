cd535ea3c5d2a2ecda90dc864d5401c7e48d96b9
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2016-03-02 12:09:43 -0600

    added a comment about the validity of social security numbers

--- scrubadub/filth/ssn.py
+++ scrubadub/filth/ssn.py
@@ -6,6 +6,10 @@ from .base import RegexFilth
 class SSNFilth(RegexFilth):
     type = 'ssn'
 
+    # please note that this not only captures valid SSNs but also invalid ones.
+    # This choice is delibrate in that we want to be biased toward replacing
+    # any filth with a cleaner alternative.
+    # https://en.wikipedia.org/wiki/Social_Security_number#Valid_SSNs
     regex = re.compile((
         "[0-9][0-9][0-9]"       # first three digits
         "[\-. ]"                # separator
