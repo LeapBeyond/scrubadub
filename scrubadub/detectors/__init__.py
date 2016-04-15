d94473aece7d926ecd47b03e73a739728dd8f467
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2016-04-15 08:46:33 -0500

    added all of the filth and detector objects using import_magic

--- scrubadub/detectors/__init__.py
+++ scrubadub/detectors/__init__.py
@@ -1,52 +1,29 @@
-import inspect
-import glob
 import os
-import importlib
+
+from ..import_magic import iter_subclasses, update_locals
+from .base import Detector
 
 
-def _iter_module_names():
-    this_filename = os.path.abspath(__file__)
-    this_dir = os.path.dirname(this_filename)
-    exclude_filenames = set((
-        this_filename,
-        this_filename.replace('.pyc', '.py'),
-        os.path.join(this_dir, 'base.py'),
-    ))
-    for py_filename in sorted(glob.glob(os.path.join(this_dir, '*.py'))):
-        if py_filename not in exclude_filenames:
-            filename_root, _ = os.path.splitext(py_filename)
-            module_name = os.path.basename(filename_root)
-            yield module_name
+def _is_abstract_detector(detector_cls):
+    """Detectors must define a ``filth_cls`` class attribute"""
+    return detector_cls.filth_cls is None
 
 
-# inspect all modules in this directory for subclasses of
-# inherit from BaseRecommender. inpiration from
-# http://stackoverflow.com/q/1796180/564709
-def _get_detector(module_name):
-    from .base import Detector, RegexDetector
-    base_detectors = set([Detector, RegexDetector])
-    module = importlib.import_module('.' + module_name, __package__)
-    for name, obj in inspect.getmembers(module):
-        if inspect.isclass(obj) and issubclass(obj, Detector) \
-                and obj not in base_detectors:
-            return obj
-
-
-def iter_detector_clss():
+def iter_detectors():
     """Iterate over all of the detectors that are included in this sub-package.
     This is a convenience method for capturing all new Detectors that are added
     over time and it is used both by the unit tests and in the
     ``Scrubber.__init__`` method.
     """
-    for module_name in _iter_module_names():
-        detector_cls = _get_detector(module_name)
-        if detector_cls is not None:
-            yield detector_cls
+    subclass_iterator = iter_subclasses(
+        os.path.dirname(os.path.abspath(__file__)),
+        Detector,
+        _is_abstract_detector,
+    )
+    for subclass in subclass_iterator:
+        yield subclass()
 
 
 # import all of the detector classes into the local namespace to make it easy
 # to do things like `import scrubadub.detectors.NameDetector`
-# http://stackoverflow.com/a/4526709/564709
-# http://stackoverflow.com/a/511059/564709
-for _detector_cls in iter_detector_clss():
-    locals().update({type(_detector_cls()).__name__: _detector_cls})
+update_locals(locals(), iter_detectors)
--- scrubadub/filth/__init__.py
+++ scrubadub/filth/__init__.py
@@ -1,7 +1,32 @@
+import os
+import re
+
+from ..import_magic import iter_subclasses, update_locals
 from .base import Filth, MergedFilth, RegexFilth
-from .name import NameFilth
-from .email import EmailFilth
-from .url import UrlFilth
-from .phone import PhoneFilth
-from .credential import CredentialFilth
-from .skype import SkypeFilth
+
+
+def _is_abstract_filth(filth_cls):
+    """Filth must have a ``type`` defined"""
+    return filth_cls.type is None
+
+
+def iter_filths():
+    """Iterate over all of the filths that are included in this sub-package.
+    This is a convenience method for capturing all new Filth that are added
+    over time.
+    """
+    subclass_iterator = iter_subclasses(
+        os.path.dirname(os.path.abspath(__file__)),
+        Filth,
+        _is_abstract_filth,
+    )
+    for subclass in subclass_iterator:
+        if issubclass(subclass, RegexFilth):
+            m = re.finditer(r"\s+", "fake pattern string").next()
+            yield subclass(m)
+        else:
+            yield subclass()
+
+# import all of the detector classes into the local namespace to make it easy
+# to do things like `import scrubadub.detectors.NameDetector`
+update_locals(locals(), iter_filths)
