7491e94fdc15ae8b05c0237a98821e07e4d30c78
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2016-04-15 09:28:10 -0500

    fixed some odd name replacement behavior by pinning textblob version. closes #19

--- tests/test_names.py
+++ tests/test_names.py
@@ -13,3 +13,18 @@ class NameTestCase(unittest.TestCase, BaseTestCase):
         AFTER:  {{NAME}} is a cat
         """
         self.compare_before_after()
+
+    def test_no_names(self):
+        """
+        BEFORE: Hello. Please testing.
+        AFTER: Hello. Please testing.
+        """
+        self.compare_before_after()
+
+    @unittest.skip('lower names cause problems for textblob')
+    def test_lower_names(self):
+        """
+        BEFORE: sarah is a friendly person
+        AFTER: {{NAME}} is a friendly person
+        """
+        self.compare_before_after()
