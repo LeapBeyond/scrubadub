a718063c73b4229ac9badbbb9ec90016b2478db6
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2017-02-09 16:09:52 -0600

    bump in version number

--- scrubadub/__init__.py
+++ scrubadub/__init__.py
@@ -4,7 +4,7 @@ from .scrubbers import Scrubber
 from . import filth
 from . import detectors
 
-__version__ = VERSION = "1.1.0"
+__version__ = VERSION = "1.1.1"
 
 
 def clean(text, cls=None, **kwargs):
