7bf4360d4e05fedb49c0cf0cc106fb76fad12c07
Thomas Bird <thomas.bird@leapbeyond.ai>
2020-09-15 15:33:51 +0100

    allow the regex to be in the detector or the filth

--- scrubadub/detectors/base.py
+++ scrubadub/detectors/base.py
@@ -12,13 +12,20 @@ class Detector(object):
 
 
 class RegexDetector(Detector):
+    """Base class to match PII with a regex"""
+    regex = None
 
     def iter_filth(self, text):
         if not issubclass(self.filth_cls, RegexFilth):
             raise exceptions.UnexpectedFilth(
                 'RegexFilth required for RegexDetector'
             )
-        if self.filth_cls.regex is None:
-            return
-        for match in self.filth_cls.regex.finditer(text):
+
+        # Allow the regex to be in the detector as well
+        # as the filth class
+        if self.regex is None:
+            if self.filth_cls.regex is None:
+                return
+            self.regex = self.filth_cls.regex
+        for match in self.regex.finditer(text):
             yield self.filth_cls(match)
