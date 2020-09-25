a8bd1650d4bbc5ce824856ea510e07107d4b7ce7
Thomas Bird <thomas.bird@leapbeyond.ai>
2020-09-25 20:03:37 +0100

    simplify function, rename, add a fucntion to get a summary dataframe

--- scrubadub/comparison.py
+++ scrubadub/comparison.py
@@ -1,71 +1,123 @@
 
 from . import filth
+from .filth import Filth
 from . import detectors
 
-import typing
+from typing import List, Dict, Union, Optional
 import pandas as pd
 import sklearn.metrics
 
 
-def compare_detectors_to_known_types(
-        filth_list: typing.List[filth.Filth],
-        detector_list: typing.List[detectors.Detector],
+def get_filth_classification_report(
+        filth_list: List[filth.Filth],
         output_dict: bool = False,
-) -> typing.Dict[str, float]:
+) -> Optional[Union[str, Dict[str, float]]]:
     """Compares how much filth is detected by various detectors"""
-    results = []  # type: List[List[float]]
-    filth_types = [det.filth_cls.type for det in detector_list]  # type: List[str]
-    if filth.KnownFilth.type in filth_types:
-        filth_types.pop(filth_types.index(filth.KnownFilth.type))
 
-    filth_types_to_search = [(t, None) for t in filth_types]  # type: List[Tuple[str, Optional[str]]]
-    filth_types_to_search += [(filth.KnownFilth.type, t[0]) for t in filth_types_to_search]
+    results = []  # type: List[Dict[str, int]]
 
     for filth_item in filth_list:
         sub_filths = [filth_item]
         if isinstance(filth_item, filth.base.MergedFilth):
             sub_filths = filth_item.filths
 
-        filth_contains_classes = []
-        for filth_type_to_match, comparison_type_to_match in filth_types_to_search:
-            found_type_in_sub_filths = False
-            for sub_filth in sub_filths:
-                # If we found a predefined sub_filth and it specifies a comparison type, then we
-                # also require that this matches the filth class.
-                if isinstance(sub_filth, filth.KnownFilth) and sub_filth.comparison_type is not None:
-                    found_type_in_sub_filths |= (
-                        sub_filth.type == filth_type_to_match and
-                        sub_filth.comparison_type == comparison_type_to_match
-                    )
-                else:
-                    found_type_in_sub_filths |= (sub_filth.type == filth_type_to_match)
-            filth_contains_classes.append(float(found_type_in_sub_filths))
+        results_row = {}
+        for sub_filth in sub_filths:
+            if isinstance(sub_filth, filth.KnownFilth) and sub_filth.comparison_type is not None:
+                results_row['{}:{}'.format(sub_filth.comparison_type, filth.KnownFilth.type)] = 1
+            else:
+                results_row['{}:{}'.format(sub_filth.type, sub_filth.detector_name)] = 1
+
         # Dont include filth that was not produced by one of the detectors of interest
-        if sum(filth_contains_classes) > 0:
-            results.append(filth_contains_classes)
+        if sum(results_row.values()) > 0:
+            results.append(results_row)
 
     if len(results) == 0:
-        return {}
+        return None
 
-    results_df = pd.DataFrame(results, columns=pd.MultiIndex.from_tuples(filth_types_to_search))
+    results_df = pd.DataFrame(results).fillna(0).astype(int)
+    results_df.columns = pd.MultiIndex.from_tuples(
+        results_df.columns.str.split(':').values.tolist(),
+        names=['filth_type', 'detector_name'],
+    )
 
-    detected_classes = results_df.loc[:, [x for x in results_df.columns if x[0] != filth.KnownFilth.type]].values
-    true_classes = results_df.loc[:, [x for x in results_df.columns if x[0] == filth.KnownFilth.type]].values
+    known_types = [x[0] for x in results_df.columns if x[1] == filth.KnownFilth.type]
+    detected_columns = [
+        x for x in results_df.columns
+        if x[1] != filth.KnownFilth.type and x[0] in known_types
+    ]
+    detected_classes = results_df.loc[:, detected_columns].values
+    true_classes = results_df.loc[:, [(x[0], filth.KnownFilth.type) for x in detected_columns]].values
 
-    extra_args = {}
+    if not output_dict:
+        detector_name_max_length = max([len(x[1]) for x in detected_columns]) + 4
+        class_labels = ["{} {}".format(x[0], x[1].rjust(detector_name_max_length)) for x in detected_columns]
+    else:
+        class_labels = ["{}:{}".format(*x) for x in detected_columns]
+
+    report_labels = []
     # If there is only one label reshape the data so that
     # the classification_report interprets it less ambiguously
     if detected_classes.shape[1] == 1:
         detected_classes = detected_classes.T[0]
         true_classes = true_classes.T[0]
-        extra_args['labels'] = [1]
+        report_labels = [1]
+    else:
+        report_labels = [class_labels.index(x) for x in sorted(class_labels)]
+        class_labels = sorted(class_labels)
 
     report = sklearn.metrics.classification_report(
         true_classes,
         detected_classes,
         output_dict=output_dict,
         zero_division=0,
-        target_names=[x[0] for x in results_df.columns if x[0] != filth.KnownFilth.type],
-        **extra_args
+        target_names=class_labels,
+        labels=report_labels,
+        # **extra_args
     )
     return report
+
+
+def get_filth_dataframe(filth_list: List[Filth]) -> pd.DataFrame:
+    """Returns a dataframe with the """
+    results = []
+    for group_id, filth_item in enumerate(filth_list):
+        sub_filths = [filth_item]
+        if isinstance(filth_item, filth.base.MergedFilth):
+            sub_filths = filth_item.filths
+        for filth_id, sub_filth in enumerate(sub_filths):
+            results.append({
+                'group_id': group_id,
+                'filth_id': filth_id,
+                'filth_type': sub_filth.type,
+                'detector_name': getattr(sub_filth, 'detector_name', pd.NA),
+                'document_name': getattr(sub_filth, 'document_name', pd.NA),
+                'text': sub_filth.text,
+                'beg': sub_filth.beg,
+                'end': sub_filth.end,
+                'known_filth': isinstance(sub_filth, filth.KnownFilth),
+                'comparison_type': getattr(sub_filth, 'comparison_type', pd.NA),
+            })
+
+    results_df = pd.DataFrame(results)
+    suffix_label = '_y_suffix'
+
+    return (
+        pd.merge(
+            results_df[~results_df['known_filth']],
+            results_df[results_df['known_filth']][['group_id', 'text', 'beg', 'end', 'comparison_type']],
+            how='outer',
+            left_on=('group_id', 'filth_type'),
+            right_on=('group_id', 'comparison_type'),
+            suffixes=('', suffix_label)
+        )
+        .rename(columns=lambda x: x if not x.endswith(suffix_label) else 'known_' + x[:-len(suffix_label)])
+        .assign(
+            known_filth=lambda df: ~pd.isnull(df['known_text']),
+            exact_match=lambda df: (df['text'] == df['known_text']).fillna(False),
+            partial_match=lambda df: ((df['beg'] < df['known_end']) & (df['end'] > df['known_beg']).fillna(False)),
+            true_positive=lambda df: (~pd.isnull(df['known_text'])) & (~pd.isnull(df['text'])),
+            false_positive=lambda df: (pd.isnull(df['known_text'])) & (~pd.isnull(df['text'])),
+            false_negative=lambda df: (~pd.isnull(df['known_text'])) & (pd.isnull(df['text'])),
+        )
+    )
--- tests/test_comparison.py
+++ tests/test_comparison.py
@@ -15,27 +15,27 @@ class ComparisonTestCase(unittest.TestCase):
         """test basic comparison"""
         filths = [
             MergedFilth(
-                PhoneFilth(beg=0, end=4, text='1234'),
+                PhoneFilth(beg=0, end=4, text='1234', detector_name='phone'),
                 KnownFilth(beg=0, end=4, text='1234', comparison_type='phone'),
             ),
             KnownFilth(beg=5, end=10, text='12345', comparison_type='phone'),
             MergedFilth(
-                PhoneFilth(beg=5, end=9, text='1234'),
+                PhoneFilth(beg=5, end=9, text='1234', detector_name='phone'),
                 KnownFilth(beg=5, end=9, text='1234', comparison_type='phone'),
             ),
             KnownFilth(beg=15, end=20, text='12345', comparison_type='phone'),
         ]
 
         self.assertEquals(
-            scrubadub.comparison.compare_detectors_to_known_types(
+            scrubadub.comparison.get_filth_classification_report(
                 filths,
-                [PhoneDetector, KnownFilthDetector],
+                # [PhoneDetector, KnownFilthDetector],
                 output_dict=True,
             ),
            {
                'macro avg': {'f1-score': 0.6666666666666666, 'precision': 1.0,'recall': 0.5, 'support': 4},
                'micro avg': {'f1-score': 0.6666666666666666, 'precision': 1.0, 'recall': 0.5, 'support': 4},
-               'phone': {'f1-score': 0.6666666666666666, 'precision': 1.0, 'recall': 0.5, 'support': 4},
+               'phone:phone': {'f1-score': 0.6666666666666666, 'precision': 1.0, 'recall': 0.5, 'support': 4},
                'weighted avg': {'f1-score': 0.6666666666666666, 'precision': 1.0, 'recall': 0.5, 'support': 4}
            },
         )
@@ -43,23 +43,23 @@ class ComparisonTestCase(unittest.TestCase):
     def test_false_positive(self):
         """test with incorrect identification"""
         filths = [
-            PhoneFilth(beg=0, end=4, text='1234'),
+            PhoneFilth(beg=0, end=4, text='1234', detector_name='phone_v1'),
             KnownFilth(beg=5, end=10, text='12345', comparison_type='phone'),
             MergedFilth(
-                PhoneFilth(beg=5, end=9, text='1234'),
+                PhoneFilth(beg=5, end=9, text='1234', detector_name='phone_v1'),
                 KnownFilth(beg=5, end=9, text='1234', comparison_type='phone'),
             ),
             KnownFilth(beg=15, end=20, text='12345', comparison_type='phone'),
         ]
 
         self.assertEquals(
-            scrubadub.comparison.compare_detectors_to_known_types(
+            scrubadub.comparison.get_filth_classification_report(
                 filths,
-                [PhoneDetector, KnownFilthDetector],
+                # [PhoneDetector, KnownFilthDetector],
                 output_dict=True,
             ),
             {
-                'phone': {'precision': 0.5, 'recall': 0.3333333333333333, 'f1-score': 0.4, 'support': 3},
+                'phone:phone_v1': {'precision': 0.5, 'recall': 0.3333333333333333, 'f1-score': 0.4, 'support': 3},
                 'micro avg': {'precision': 0.5, 'recall': 0.3333333333333333, 'f1-score': 0.4, 'support': 3},
                 'macro avg': {'precision': 0.5, 'recall': 0.3333333333333333, 'f1-score': 0.4, 'support': 3},
                 'weighted avg': {'precision': 0.5, 'recall': 0.3333333333333333, 'f1-score': 0.4000000000000001, 'support': 3}
@@ -77,26 +77,25 @@ class ComparisonTestCase(unittest.TestCase):
 
         filths = [
             MergedFilth(
-                PhoneFilth(beg=0, end=4, text='1234'),
+                PhoneFilth(beg=0, end=4, text='1234', detector_name='phone'),
                 KnownFilth(beg=0, end=4, text='1234', comparison_type='phone'),
             ),
             KnownFilth(beg=5, end=10, text='12345', comparison_type='phone'),
             MergedFilth(
-                TempFilth(beg=5, end=9, text='1234'),
+                TempFilth(beg=5, end=9, text='1234', detector_name='temp'),
                 KnownFilth(beg=5, end=9, text='1234', comparison_type='temp'),
             ),
             KnownFilth(beg=15, end=20, text='12345', comparison_type='temp'),
         ]
 
         self.assertEquals(
-            scrubadub.comparison.compare_detectors_to_known_types(
+            scrubadub.comparison.get_filth_classification_report(
                 filths,
-                [PhoneDetector, TempDetector, KnownFilthDetector],
                 output_dict=True,
             ),
             {
-                'phone': {'precision': 1.0, 'recall': 0.5, 'f1-score': 0.6666666666666666, 'support': 2},
-                'temp': {'precision': 1.0, 'recall': 0.5, 'f1-score': 0.6666666666666666, 'support': 2},
+                'phone:phone': {'precision': 1.0, 'recall': 0.5, 'f1-score': 0.6666666666666666, 'support': 2},
+                'temp:temp': {'precision': 1.0, 'recall': 0.5, 'f1-score': 0.6666666666666666, 'support': 2},
                 'micro avg': {'precision': 1.0, 'recall': 0.5, 'f1-score': 0.6666666666666666, 'support': 4},
                 'macro avg': {'precision': 1.0, 'recall': 0.5, 'f1-score': 0.6666666666666666, 'support': 4},
                 'weighted avg': {'precision': 1.0, 'recall': 0.5, 'f1-score': 0.6666666666666666, 'support': 4},
@@ -108,23 +107,25 @@ class ComparisonTestCase(unittest.TestCase):
         """test comparison with other predefined filth types"""
         filths = [
             MergedFilth(
-                PhoneFilth(beg=0, end=4, text='John'),
+                PhoneFilth(beg=0, end=4, text='John', detector_name='phone'),
                 KnownFilth(beg=0, end=4, text='John', comparison_type='phone')
             ),
             KnownFilth(beg=5, end=10, text='Hello', comparison_type='word'),
         ]
 
         self.assertEquals(
-            scrubadub.comparison.compare_detectors_to_known_types(
+            scrubadub.comparison.get_filth_classification_report(
                 filths,
-                [PhoneDetector, KnownFilthDetector],
+                # [PhoneDetector, KnownFilthDetector],
                 output_dict=True,
             ),
             {
-                'phone': {'precision': 1.0, 'recall': 1.0, 'f1-score': 1.0, 'support': 1},
-                'accuracy': 1.0, 'macro avg': {'precision': 1.0, 'recall': 1.0, 'f1-score': 1.0, 'support': 1},
+                'phone:phone': {'precision': 1.0, 'recall': 1.0, 'f1-score': 1.0, 'support': 1},
+                'micro avg': {'precision': 1.0, 'recall': 1.0, 'f1-score': 1.0, 'support': 1},
+                'macro avg': {'precision': 1.0, 'recall': 1.0, 'f1-score': 1.0, 'support': 1},
                 'weighted avg': {'precision': 1.0, 'recall': 1.0, 'f1-score': 1.0, 'support': 1}
             },
+
         )
 
     def test_with_irrelevant_filth(self):
@@ -135,22 +136,23 @@ class ComparisonTestCase(unittest.TestCase):
 
         filths = [
             MergedFilth(
-                PhoneFilth(beg=0, end=4, text='John'),
+                PhoneFilth(beg=0, end=4, text='John', detector_name='phone'),
                 KnownFilth(beg=0, end=4, text='John', comparison_type='phone')
             ),
             KnownFilth(beg=5, end=10, text='Hello', comparison_type='name'),
-            KnownFilth(beg=5, end=10, text='Hello', comparison_type='temp'),
-            TempFilth(beg=100, end=103, text='123'),
+            # KnownFilth(beg=5, end=10, text='Hello', comparison_type='temp'),
+            TempFilth(beg=100, end=103, text='123', detector_name='temp'),
         ]
 
         self.assertEquals(
-            scrubadub.comparison.compare_detectors_to_known_types(
+            scrubadub.comparison.get_filth_classification_report(
                 filths,
-                [PhoneDetector, KnownFilthDetector],
+                # [PhoneDetector, KnownFilthDetector],
                 output_dict=True,
             ),
             {
-                'phone': {'precision': 1.0, 'recall': 1.0, 'f1-score': 1.0, 'support': 1}, 'accuracy': 1.0,
+                'phone:phone': {'precision': 1.0, 'recall': 1.0, 'f1-score': 1.0, 'support': 1},
+                'micro avg': {'precision': 1.0, 'recall': 1.0, 'f1-score': 1.0, 'support': 1},
                 'macro avg': {'precision': 1.0, 'recall': 1.0, 'f1-score': 1.0, 'support': 1},
                 'weighted avg': {'precision': 1.0, 'recall': 1.0, 'f1-score': 1.0, 'support': 1}
             },
