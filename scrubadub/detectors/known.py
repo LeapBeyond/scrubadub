a4e526ddb5aab13109c49419b1fde5e17a41c4a0
Thomas Bird <thomas.bird@leapbeyond.ai>
2020-09-28 12:34:07 +0100

    add document_name and detector_name into arguments of initalised filth

--- scrubadub/detectors/known.py
+++ scrubadub/detectors/known.py
@@ -1,7 +1,22 @@
-import typing
+import sys
+
+from typing import Optional, List, Union, Dict, Generator
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict  # pylint: disable=no-name-in-module
+else:
+    from typing_extensions import TypedDict
 
 from .base import Detector
 from ..filth.known import KnownFilth
+
+
+class KnownFilthItem(TypedDict):
+    start: str
+    end: str
+    limit: int
+    match: str
+    comparison_type: str
 
 
 class KnownFilthDetector(Detector):
@@ -13,19 +28,21 @@ class KnownFilthDetector(Detector):
     """
 
     filth_cls = KnownFilth
+    name = 'known'
 
-    def __init__(self, predefined_pii: typing.Optional[typing.List[typing.Dict[str, str]]] = None):
-        super().__init__()
+    def __init__(self, predefined_pii: Optional[List[KnownFilthItem]] = None, **kwargs):
+        super().__init__(**kwargs)
         if predefined_pii is None:
             predefined_pii = []
         self._predefined_pii = predefined_pii
 
-    @staticmethod
     def _find_all(
-            text,
-            substr,
-            comparison_type: typing.Optional[str] = None,
-    ) -> typing.Generator[KnownFilth, None, None]:
+            self,
+            text: str,
+            substr: str,
+            comparison_type: Optional[str] = None,
+            document_name: Optional[str] = None
+    ) -> Generator[KnownFilth, None, None]:
         """Yield filth for each match to substr in text."""
         substr_len = len(substr)
         start_location = text.find(substr)
@@ -36,20 +53,23 @@ class KnownFilthDetector(Detector):
                 start_location + substr_len,
                 text[start_location:start_location + substr_len],
                 comparison_type=comparison_type,
+                detector_name=self.name,
+                document_name=document_name,
             )
             start_location = text.find(
                 substr,
                 start_location + substr_len
             )
 
-    @staticmethod
     def _find_all_between(
+            self,
             text: str,
             substr_start: str,
             substr_end: str,
             limit: int = 150,
-            comparison_type: typing.Optional[str] = None,
-    ) -> typing.Generator[KnownFilth, None, None]:
+            comparison_type: Optional[str] = None,
+            document_name: Optional[str] = None
+    ) -> Generator[KnownFilth, None, None]:
         """Yield filth for text between (and including)
         substr_start and substr_end, but only if the text
         between the two is less than limit characters.
@@ -70,6 +90,8 @@ class KnownFilthDetector(Detector):
                     end_location + substr_end_len,
                     text[start_location:end_location + substr_end_len],
                     comparison_type=comparison_type,
+                    detector_name=self.name,
+                    document_name=document_name,
                 )
                 next_search_start = end_location + substr_end_len
             else:
@@ -79,8 +101,9 @@ class KnownFilthDetector(Detector):
 
     def iter_filth(
             self,
-            text: str
-    ) -> typing.Generator[KnownFilth, None, None]:
+            text: str,
+            document_name: Optional[str] = None
+    ) -> Generator[KnownFilth, None, None]:
         """Iterate over the predefined PII list and yield
         filth instances."""
         for pii_item in self._predefined_pii:
@@ -89,7 +112,8 @@ class KnownFilthDetector(Detector):
                 for found_item in self._find_all(
                         text,
                         pii_item['match'],
-                        comparison_type=pii_item.get('comparison_type', None)
+                        comparison_type=pii_item.get('comparison_type', None),
+                        document_name=document_name,
                 ):
                     yield found_item
             elif 'start' in pii_item and 'end' in pii_item:
@@ -97,8 +121,9 @@ class KnownFilthDetector(Detector):
                     text,
                     pii_item['start'],
                     pii_item['end'],
-                    limit=pii_item.get('limit', 150),
-                    comparison_type=pii_item.get('comparison_type', None)
+                    limit=int(pii_item.get('limit', 150)),
+                    comparison_type=pii_item.get('comparison_type', None),
+                    document_name=document_name,
                 ):
                     yield found_item
             else:
