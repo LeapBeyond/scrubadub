6b9b73f3f711aa79b6fe9f917d7e1f04218b46cd
Thomas Bird <thomas.bird@leapbeyond.ai>
2020-09-28 15:40:49 +0100

    remove the autoimport code

--- scrubadub/detectors/__init__.py
+++ scrubadub/detectors/__init__.py
@@ -1,4 +1,3 @@
-import os
 import sys
 
 from textblob.blob import BaseBlob
@@ -10,7 +9,6 @@ if sys.version_info >= (3, 8):
 else:
     from typing_extensions import TypedDict
 
-from ..import_magic import iter_subclasses, update_locals
 from .base import Detector
 from .credential import CredentialDetector
 from .email import EmailDetector, NewEmailDetector
@@ -63,33 +61,3 @@ def register_detector(detector: Type[Detector], autoload: bool = False):
         'detector': detector,
         'autoload': autoload,
     }
-
-
-def _is_abstract_detector(detector_cls):
-    """Detectors must define a ``filth_cls`` class attribute"""
-    return detector_cls.filth_cls is None
-
-
-def iter_detector_clss():
-    """Iterate over all of the detectors that are included in this sub-package.
-    This is a convenience method for capturing all new Detectors that are added
-    over time and it is used both by the unit tests and in the
-    ``Scrubber.__init__`` method.
-    """
-    return iter_subclasses(
-        os.path.dirname(os.path.abspath(__file__)),
-        Detector,
-        _is_abstract_detector,
-    )
-
-
-def iter_detectors():
-    """Iterate over all instances of ``Detector`` subclasses.
-    """
-    for detector_cls in iter_detector_clss():
-        yield detector_cls()
-
-
-# import all of the detector classes into the local namespace to make it easy
-# to do things like `import scrubadub.detectors.NameDetector`
-# update_locals(locals(), iter_detectors)
--- scrubadub/filth/__init__.py
+++ scrubadub/filth/__init__.py
@@ -1,7 +1,5 @@
 import os
-import re
 
-from ..import_magic import iter_subclasses, update_locals
 from .base import Filth, MergedFilth
 from .address import AddressFilth
 from .credential import CredentialFilth
@@ -13,31 +11,3 @@ from .phone import PhoneFilth
 from .skype import SkypeFilth
 from .ssn import SSNFilth
 from .url import UrlFilth
-
-
-def _is_abstract_filth(filth_cls):
-    """Filth must have a ``type`` defined"""
-    return filth_cls.type is None
-
-
-def iter_filth_clss():
-    """Iterate over all of the filths that are included in this sub-package.
-    This is a convenience method for capturing all new Filth that are added
-    over time.
-    """
-    return iter_subclasses(
-        os.path.dirname(os.path.abspath(__file__)),
-        Filth,
-        _is_abstract_filth,
-    )
-
-
-def iter_filths():
-    """Iterate over all instances of filth"""
-    for filth_cls in iter_filth_clss():
-        yield filth_cls()
-
-
-# import all of the detector classes into the local namespace to make it easy
-# to do things like `import scrubadub.detectors.NameDetector`
-# update_locals(locals(), iter_filths)
