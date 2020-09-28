dfefb4b3caeb10b7ae765c968916ce2a25f94dcb
Thomas Bird <thomas.bird@leapbeyond.ai>
2020-09-28 13:16:32 +0100

    test detector names instead of regex class

--- tests/test_detector.py
+++ tests/test_detector.py
@@ -1,31 +1,23 @@
 import unittest
 
 import scrubadub
-from scrubadub.detectors.base import RegexDetector
-from scrubadub.filth.base import Filth
-from scrubadub.exceptions import UnexpectedFilth
+from scrubadub.detectors.url import UrlDetector
+from scrubadub.detectors.email import EmailDetector
 
 
-class RegexDetectorTestCase(unittest.TestCase):
+class DetectorTestCase(unittest.TestCase):
+    pass
+    # TODO: test detector names
 
-    def test_regex_filth(self):
-        """make sure RegexDetector only works with RegexFilth"""
+    def test_detector_names(self):
+        """make sure detector names appear in Filth"""
+        detector = UrlDetector(name='example_name')
+        filths = list(detector.iter_filth('www.google.com'))
+        self.assertEqual(len(filths), 1)
+        self.assertEqual(filths[0].detector_name, 'example_name')
 
-        class MyFilth(Filth):
-            pass
+        detector = EmailDetector(name='example_name')
+        filths = list(detector.iter_filth('example@example.com'))
+        self.assertEqual(len(filths), 1)
+        self.assertEqual(filths[0].detector_name, 'example_name')
 
-        class MyDetector(RegexDetector):
-            filth_cls = MyFilth
-
-        text = 'dirty dirty text'
-        detector = MyDetector()
-        with self.assertRaises(UnexpectedFilth):
-            for filth in detector.iter_filth(text):
-                pass
-
-    def test_detector_filth_cls(self):
-        """Detector.filth_cls should always exist"""
-        for detector_cls in scrubadub.detectors.iter_detectors():
-            self.assertTrue(getattr(detector_cls, 'filth_cls', False),
-                '%s does not have a filth_cls set' % detector_cls
-            )
