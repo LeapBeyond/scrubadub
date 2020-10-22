e372b0771b77b06a704fd35454c10afce79e3e13
aCampello <a.campello@wellcome.ac.uk>
2020-10-22 23:27:57 +0100

    Add support to different types for documents

--- scrubadub/detectors/ner.py
+++ scrubadub/detectors/ner.py
@@ -1,6 +1,6 @@
 import spacy
 
-from typing import Dict, Generator, List, Optional, Set, Sequence, Union
+from typing import Dict, Generator, List, Optional, Set, Sequence, Tuple, Union
 
 from .base import Detector
 from ..filth import NERFilth, Filth
@@ -17,7 +17,7 @@ class SpacyDetector(Detector):
 
     disallowed_nouns = CanonicalStringSet(["skype"])
 
-    def __init__(self, named_entities: Optional[Union[List[str], Set[str]]] = {'PERSON'},
+    def __init__(self, named_entities: Union[List[str], Set[str]] = {'PERSON'},
                  model: str = "en_core_web_trf", **kwargs):
         # Spacy NER are all upper cased
         self.named_entities = {entity.upper() for entity in named_entities}
@@ -32,13 +32,20 @@ class SpacyDetector(Detector):
 
     def iter_filth_documents(self, documents: Union[Sequence[str], Dict[str, str]]) -> Generator[Filth, None, None]:
         if isinstance(documents, list):
-            for doc in self.nlp.pipe(documents):
-                for ent in doc.ents:
-                    if ent.label_ in self.named_entities:
-                        yield self.filth_cls(beg=ent.start_char,
-                                             end=ent.end_char,
-                                             text=ent.text,
-                                             detector_name=self.name)
+            doc_names, doc_list = zip(*enumerate(documents))
+        elif isinstance(documents, dict):
+            doc_names, doc_list = zip(*documents.items())
+        else:
+            raise TypeError('documents must be one of a string, list of strings or dict of strings.')
+
+        for doc_name, doc in zip(doc_names, self.nlp.pipe(doc_list)):
+            for ent in doc.ents:
+                if ent.label_ in self.named_entities:
+                    yield self.filth_cls(beg=ent.start_char,
+                                         end=ent.end_char,
+                                         text=ent.text,
+                                         document_name=str(doc_name),
+                                         detector_name=self.name)
 
     def iter_filth(self, text: str, document_name: Optional[str] = None) -> Generator[Filth, None, None]:
         pass
