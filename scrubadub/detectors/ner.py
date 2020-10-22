b20c212c9a471bad0610fcc9f094af8826e98630
aCampello <a.campello@wellcome.ac.uk>
2020-10-22 22:59:11 +0100

    Add iter_filth_documents basic logic

--- scrubadub/detectors/ner.py
+++ scrubadub/detectors/ner.py
@@ -1,6 +1,6 @@
 import spacy
 
-from typing import List, Optional, Generator
+from typing import Dict, Generator, List, Optional, Set, Sequence, Union
 
 from .base import Detector
 from ..filth import NERFilth, Filth
@@ -9,15 +9,18 @@ from ..utils import CanonicalStringSet
 
 class SpacyDetector(Detector):
     """Use spacy's named entity recognition to clean named entities.
-     List specific entities to include passing ``named_entities``
+     List specific entities to include passing ``named_entities``, e.g.
+     (PERSON)
     """
     filth_cls = NERFilth
     name = 'spacy_ner'
 
     disallowed_nouns = CanonicalStringSet(["skype"])
 
-    def __init__(self, named_entities: Optional[List[str]] = None, model: str = "en_core_web_trf", **kwargs):
-        self.named_entities = named_entities
+    def __init__(self, named_entities: Optional[Union[List[str], Set[str]]] = {'PERSON'},
+                 model: str = "en_core_web_trf", **kwargs):
+        # Spacy NER are all upper cased
+        self.named_entities = {entity.upper() for entity in named_entities}
         if model not in spacy.info()['pipelines']:
             raise OSError(f"Can't find model '{model}'. If it is a valid Spacy model, "
                           f"download it (e.g. with the CLI command "
@@ -27,5 +30,18 @@ class SpacyDetector(Detector):
         self.nlp.select_pipes(enable=["transformer", "tagger", "parser", "ner"])
         super(SpacyDetector, self).__init__(**kwargs)
 
+    def iter_filth_documents(self, documents: Union[Sequence[str], Dict[str, str]]) -> Generator[Filth, None, None]:
+        if isinstance(documents, list):
+            for doc in self.nlp.pipe(documents):
+                for ent in doc.ents:
+                    if ent.label_ in self.named_entities:
+                        yield self.filth_cls(beg=ent.start_char,
+                                             end=ent.end_char,
+                                             text=ent.text,
+                                             detector_name=self.name)
+
     def iter_filth(self, text: str, document_name: Optional[str] = None) -> Generator[Filth, None, None]:
         pass
+
+
+
