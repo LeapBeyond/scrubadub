499955c13ab18176405643a45c3f29f67ab44f78
M Chong <mchong@ms-macbook-pro.mynet>
2020-11-03 18:53:38 +0000

    adding tests for UK NINO

--- scrubadub/detectors/uknino.py
+++ scrubadub/detectors/uknino.py
@@ -1,11 +1,17 @@
-import re, scrubadub
+import re
+
+from .base import RegexDetector
+from ..filth import UKNinoFilth
 
 
-class NewNINODetector(scrubadub.detectors.Detector):
-    name = 'NewNINODetector'
-    filth_cls = scrubadub.filth.Filth.UkNino
-    regex = re.compile(r"(?!BG)(?!GB)(?!NK)(?!KN)(?!TN)(?!NT)(?!ZZ)(?:[A-CEGHJ-PR-TW-Z][A-CEGHJ-NPR-TW-Z])(?:\s*\d\s*){6}([A-D]|\s)", re.IGNORECASE)
-    scrubber = scrubadub.Scrubber()
-    scrubber.add_detector(NewNINODetector())
-    text = u"This url will be found https://example.com, NINO: ZB 79 53 67 A"
-    scrubber.clean(text)
+class NINODetector(RegexDetector):
+    """Use regular expressions to remove the UK National Insurance number (NINO),
+    Simple pattern matching, no checksum solution.
+    """
+
+    name = 'uknino'
+    filth_cls = UKNinoFilth
+    # this regex is looking for NINO that does not begin with certain letters
+    regex = re.compile(r'''
+    (?!BG)(?!GB)(?!NK)(?!KN)(?!TN)(?!NT)(?!ZZ)(?:[A-CEGHJ-PR-TW-Z][A-CEGHJ-NPR-TW-Z])(?:\s*\d\s*){6}([A-D]|\s)''',
+                       re.IGNORECASE)
