bdd74fd84e79b2a37c3f56901e0b02cd528d4be5
Thomas Bird <thomas.bird@leapbeyond.ai>
2020-11-05 17:32:48 +0000

    move away from names and use twitter and phone detectors instead

--- tests/test_api.py
+++ tests/test_api.py
@@ -17,12 +17,12 @@ class APITestCase(unittest.TestCase):
             scrubadub.clean_documents(
                 {
                     "first.txt": "This is a test message for example@exampe.com",
-                    "second.txt": "Hello Jane I am Tom.",
+                    "second.txt": "Hello @Jane call me on +33 6 39 88 11 67.",
                 }
             ),
             {
                 "first.txt": "This is a test message for {{EMAIL}}",
-                "second.txt": "Hello {{NAME}} I am {{NAME}}.",
+                "second.txt": "Hello {{TWITTER}} call me on {{PHONE}}.",
             }
         )
 
@@ -39,7 +39,7 @@ class APITestCase(unittest.TestCase):
         filths = scrubadub.list_filth_documents(
             {
                 "first.txt": "This is a test message for example@example.com",
-                "second.txt": "Hello Jane, I am Tom.",
+                "second.txt": "Hello @Jane call me on +33 6 39 88 11 67.",
             }
         )
         self.assertEqual(
@@ -48,8 +48,12 @@ class APITestCase(unittest.TestCase):
                 scrubadub.filth.EmailFilth(
                     text='example@example.com', document_name='first.txt', detector_name='email', beg=27, end=46
                 ),
-                scrubadub.filth.NameFilth(text='Jane', document_name='second.txt', detector_name='name', beg=6, end=10),
-                scrubadub.filth.NameFilth(text='Tom', document_name='second.txt', detector_name='name', beg=17, end=20),
+                scrubadub.filth.TwitterFilth(
+                    text='@Jane', document_name='second.txt', detector_name='twitter', beg=6, end=11
+                ),
+                scrubadub.filth.PhoneFilth(
+                    text='+33 6 39 88 11 67', document_name='second.txt', detector_name='phone', beg=23, end=40
+                ),
             ])
         )
 
