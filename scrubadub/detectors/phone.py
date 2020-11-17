0d0aa146ca3a9dca3c6fc197eeca7fdd25972409
Thomas Bird <thomas.bird@leapbeyond.ai>
2020-11-17 18:16:04 +0000

    pass the locale to the filth, advertise that the PhoneDetector supports all locales

--- scrubadub/detectors/phone.py
+++ scrubadub/detectors/phone.py
@@ -19,10 +19,6 @@ class PhoneDetector(Detector):
     filth_cls = PhoneFilth
     name = 'phone'
 
-    def __init__(self, region: str = 'US', **kwargs):
-        super(PhoneDetector, self).__init__(**kwargs)
-        self.region = region
-
     def iter_filth(self, text, document_name: Optional[str] = None):
         # create a copy of text to handle multiple phone numbers correctly
         for match in phonenumbers.PhoneNumberMatcher(text, self.region):
@@ -31,5 +27,18 @@ class PhoneDetector(Detector):
                 end=match.end,
                 text=match.raw_string,
                 detector_name=self.name,
-                document_name=document_name
+                document_name=document_name,
+                locale=self.locale,
             )
+
+    @classmethod
+    def supported_locale(cls, locale: str) -> bool:
+        """Returns true if this ``Detector`` supports the given locale.
+
+        :param locale: The locale of the documents in the format: 2 letter lower-case language code followed by an
+                       underscore and the two letter upper-case country code, eg "en_GB" or "de_CH".
+        :type locale: str
+        :return: ``True`` if the locale is supported, otherwise ``False``
+        :rtype: bool
+        """
+        return True
