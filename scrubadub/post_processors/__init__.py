982669830c92d751dc7cbac26cd0fe22fcf370ca
Thomas Bird <thomas.bird@leapbeyond.ai>
2020-11-17 18:22:18 +0000

    improve docs, ensure all PostProcessors are in the post_processors module

--- scrubadub/post_processors/__init__.py
+++ scrubadub/post_processors/__init__.py
@@ -26,20 +26,29 @@ post_processor_configuration = {
 }  # type: Dict[str, PostProcessorConfigurationItem]
 
 
-def register_post_processor(detector: Type[PostProcessor], autoload: bool = False, index: int = 0):
+def register_post_processor(post_processor: Type[PostProcessor], autoload: bool = False, index: int = 0):
     """Register a PostProcessor for use with the ``Scrubber`` class.
 
-    This is used when you dont want to have to import a detector by default.
-    It may be useful for certain detectors with large or unusal dependancies, which you may not always want to import.
+    This is used when you don't want to have to import a detector by default.
+    It may be useful for certain detectors with large or unusual dependencies, which you may not always want to import.
     In this case you can use ``register_detector(NewDetector, autoload=True)`` after your detector definition so that
     if the file is imported it wil be automatically registered.
     This will mean that you don't need to import the ``NewDetector`` in this file and so it's dependencies won't need
     to be installed just to import this package.
 
     The argument ``autoload`` sets if a new ``Scrubber()`` instance should load this ``Detector`` by default.
+
+    :param post_processor: The ``PostProcessor`` to register with the scrubadub post-processor configuration.
+    :type post_processor: PostProcessor class
+    :param autoload: Whether to automatically load this ``Detector`` on ``Scrubber`` initialisation.
+    :type autoload: bool
+    :param index: The location/index in which this ``PostProcessor`` should be added.
+    :type index: int
     """
-    post_processor_configuration[detector.name] = {
-        'post_processor': detector,
+    post_processor_configuration[post_processor.name] = {
+        'post_processor': post_processor,
         'autoload': autoload,
         'index': index,
     }
+    current_module = __import__(__name__)
+    setattr(current_module.post_processors, post_processor.__name__, post_processor)
