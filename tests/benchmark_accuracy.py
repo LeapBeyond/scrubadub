62bda4128fb74a57513f0f66a301c0b3965c9214
Thomas Bird <thomas.bird@leapbeyond.ai>
2020-11-17 18:26:47 +0000

    tottal rewrite of the accuracy script to bring it up to scratch

--- tests/benchmark_accuracy.py
+++ tests/benchmark_accuracy.py
@@ -1,109 +1,181 @@
-import sys
+#!/usr/bin/env python3
+
 import time
+import copy
+import click
+import random
+
+from faker import Faker
+from typing import List, Union, Dict
 
 import scrubadub
-
 from scrubadub.comparison import make_fake_document, get_filth_classification_report
 
-run_spacy = False
-run_stanford = False
-run_address = False
 
-run_slow = False
-
-try:
-    import scrubadub.detectors.spacy
-    run_spacy = True
-except ImportError:
-    pass
-try:
-    import scrubadub.detectors.stanford_ner
-    run_stanford = True
-except ImportError:
-    pass
-try:
-    import scrubadub.detectors.address
-    run_address = True
-except ImportError:
-    pass
+FILTH_IN_LOCALES = {
+    'en_US': ['address', 'email', 'name', 'phone', 'ssn', 'twitter', 'url'],
+    'en_GB': ['address', 'phone', 'postalcode'],
+}
 
 
-def main():
-    general_docs = []
-    known_general_pii = []
-    address_docs = []
-    known_address_pii = []
-    # uk_phone_docs = []
-    # known_uk_phone_pii = []
+def generate_and_scrub(locale, filth_list, detectors, n_docs: int = 50):
+    documents = []
+    known_pii = []
+
+    click.echo("Generating {} docs with filth: {}".format(locale, ", ".join(filth_list)))
     start_time = time.time()
-    for i_doc in range(50):
-        filth_types = ['email', 'name', 'phone', 'postalcode', 'ssn', 'twitter', 'url']
-        new_doc, new_known_pii = make_fake_document(paragraphs=2*len(filth_types), seed=i_doc, filth_types=filth_types)
-        general_docs.append(new_doc)
-        known_general_pii += new_known_pii
 
-        #new_doc, new_known_pii = make_fake_document(paragraphs=4, seed=i_doc, filth_types=['name'])
-        # Change the filth name to allow for comparison with NamedEntityDetector. Probably there is a better way to do it
-
-        #for pii in new_known_pii:
-       #     pii['filth_type'] = 'named_entity'
-
-        #named_entity_docs.append(new_doc)
-        #known_named_entity_pii += new_known_pii
-
-        if run_address:
-            new_doc, new_known_pii = make_fake_document(paragraphs=4, seed=i_doc, filth_types=['gb_address', 'us_address'])
-            address_docs.append(new_doc)
-            known_address_pii += new_known_pii
-
-        # new_doc, new_known_pii = make_fake_document(
-        #     faker=faker.Faker(locale='en_gb'), paragraphs=2, seed=i_doc, filth_types=['phone']
-        # )
-        # uk_phone_docs.append(new_doc)
-        # known_uk_phone_pii += new_known_pii
+    for i_doc in range(n_docs):
+        new_doc, new_known_pii = make_fake_document(paragraphs=2*len(filth_list), locale=locale, seed=None,
+                                                    filth_types=filth_list)
+        documents.append(new_doc)
+        known_pii += new_known_pii
 
     scrubber_time = time.time()
-    scrubber = scrubadub.Scrubber()
-    if run_spacy:
-        scrubber.add_detector(scrubadub.detectors.spacy.SpacyEntityDetector(name='en_core_web_sm', model='en_core_web_sm'))
-        if run_slow:
-            scrubber.add_detector(scrubadub.detectors.spacy.SpacyEntityDetector(name='en_core_web_md', model='en_core_web_md'))
-            scrubber.add_detector(scrubadub.detectors.spacy.SpacyEntityDetector(name='en_core_web_lg', model='en_core_web_lg'))
-            scrubber.add_detector(scrubadub.detectors.spacy.SpacyEntityDetector(name='en_core_web_trf', model='en_core_web_trf'))
-    if run_stanford and run_slow:
-        scrubber.add_detector(scrubadub.detectors.stanford_ner.StanfordNERDetector())
-    scrubber.add_detector(scrubadub.detectors.KnownFilthDetector(known_filth_items=known_general_pii))
-    filth_list = list(scrubber.iter_filth_documents(general_docs))
+    click.echo("Scrubbing with detectors: {}".format(', '.join(detectors)))
 
-    if run_address:
-        scrubber = scrubadub.Scrubber(detector_list=[
-            scrubadub.detectors.address.GBAddressDetector(),
-            scrubadub.detectors.address.USAddressDetector(),
-            scrubadub.detectors.KnownFilthDetector(known_filth_items=known_address_pii),
-        ])
-        filth_list += list(scrubber.iter_filth_documents(address_docs))
-
-    # scrubber = scrubadub.Scrubber(detector_list=[
-    #     scrubadub.detectors.PhoneDetector(region='gb', name='gb_phone'),
-    #     scrubadub.detectors.KnownFilthDetector(known_filth_items=known_uk_phone_pii),
-    # ])
-    # filth_list += list(scrubber.iter_filth_documents(uk_phone_docs))
+    detectors.append(scrubadub.detectors.KnownFilthDetector(locale=locale, known_filth_items=known_pii))
+    scrubber = scrubadub.Scrubber(locale=locale, detector_list=detectors)
+    found_filth = list(scrubber.iter_filth_documents(documents))
 
     end_time = time.time()
-    print("Documents generated in {:.2f}s".format(scrubber_time-start_time))
-    print("Scrubbed documents in  {:.2f}s".format(end_time-scrubber_time))
-    print(get_filth_classification_report(filth_list))
+    click.echo("Documents generated in {:.2f}s".format(scrubber_time-start_time))
+    click.echo("Scrubbed documents in  {:.2f}s".format(end_time-scrubber_time))
 
-    # scrubber_time = time.time()
-    # scrubber = scrubadub.Scrubber(detector_list=[scrubadub.detectors.NamedEntityDetector(),
-    #                                              scrubadub.detectors.KnownFilthDetector(known_filth_items=known_named_entity_pii)])
-    # filth_list = list(scrubber.iter_filth_documents(named_entity_docs))
-    # end_time = time.time()
-    # print("Documents generated in {:.2f}s".format(scrubber_time-start_time))
-    # print("Scrubbed documents in  {:.2f}s".format(end_time-scrubber_time))
-    # print(get_filth_classification_report(filth_list))
+    return found_filth
 
-    sys.exit(0)
+
+def document_accuracy_settings(locales: List[str], detector_available: Dict[str, bool], run_slow: bool = False):
+    global FILTH_IN_LOCALES
+    run_settings = []
+
+    for locale in locales:
+        detectors = []
+        filth_list = copy.copy(FILTH_IN_LOCALES[locale])
+
+        for filth in filth_list:
+            if filth == 'address':
+                filth_list = [x for x in filth_list if x != 'address']
+                if detector_available['address'] and run_slow:
+                    run_settings += [
+                        (locale, ['address'], ['address'])
+                    ]
+            elif filth == 'name':
+                added_name_detector = False
+                if detector_available['spacy']:
+                    detectors.append('spacy_en_core_web_sm')
+                    added_name_detector = True
+                    if run_slow:
+                        detectors += ['spacy_en_core_web_md', 'spacy_en_core_web_lg', 'spacy_en_core_web_trf', ]
+                if run_slow:
+                    if detector_available['text_blob']:
+                        detectors.append('text_blob_name')
+                        added_name_detector = True
+                    if detector_available['stanford_ner']:
+                        detectors.append('stanford_ner')
+                        added_name_detector = True
+                if not added_name_detector:
+                    filth_list = [x for x in filth_list if x != 'name']
+            else:
+                detectors.append(filth)
+
+        if len(filth_list) > 0 and len(detectors) > 0:
+            run_settings += [(locale, filth_list, detectors)]
+
+    return run_settings
+
+def load_complicated_detectors(run_slow: bool) -> Dict[str, bool]:
+    detector_available = {
+        'address': False,
+        'spacy': False,
+        'stanford_ner': False,
+        'text_blob': False,
+    }
+
+    if run_slow:
+        try:
+            import scrubadub.detectors.stanford_ner
+            detector_available['stanford_ner'] = True
+        except ImportError:
+            pass
+        try:
+            import scrubadub.detectors.address
+            detector_available['address'] = True
+        except ImportError:
+            pass
+        try:
+            import scrubadub.detectors.text_blob
+            detector_available['text_blob'] = True
+        except ImportError:
+            pass
+        try:
+            import scrubadub.detectors.spacy
+            detector_available['spacy'] = True
+        except ImportError:
+            pass
+        if detector_available['spacy']:
+            del scrubadub.detectors.detector_configuration[scrubadub.detectors.spacy.SpacyEntityDetector.name]
+
+            # TODO: this only supports english models for spacy, this should be improved
+            class SpacyEnSmDetector(scrubadub.detectors.spacy.SpacyEntityDetector):
+                name = 'spacy_en_core_web_sm'
+                def __init__(self, **kwargs):
+                    super(SpacyEnSmDetector, self).__init__(model='en_core_web_sm', **kwargs)
+
+            class SpacyEnMdDetector(scrubadub.detectors.spacy.SpacyEntityDetector):
+                name = 'spacy_en_core_web_md'
+                def __init__(self, **kwargs):
+                    super(SpacyEnMdDetector, self).__init__(model='en_core_web_md', **kwargs)
+
+            class SpacyEnLgDetector(scrubadub.detectors.spacy.SpacyEntityDetector):
+                name = 'spacy_en_core_web_lg'
+                def __init__(self, **kwargs):
+                    super(SpacyEnLgDetector, self).__init__(model='en_core_web_lg', **kwargs)
+
+            class SpacyEnTrfDetector(scrubadub.detectors.spacy.SpacyEntityDetector):
+                name = 'spacy_en_core_web_trf'
+                def __init__(self, **kwargs):
+                    super(SpacyEnTrfDetector, self).__init__(model='en_core_web_trf', **kwargs)
+
+            scrubadub.detectors.register_detector(SpacyEnSmDetector, autoload=False)
+            scrubadub.detectors.register_detector(SpacyEnMdDetector, autoload=False)
+            scrubadub.detectors.register_detector(SpacyEnLgDetector, autoload=False)
+            scrubadub.detectors.register_detector(SpacyEnTrfDetector, autoload=False)
+
+    return detector_available
+
+
+@click.command()
+@click.option('--ndocs', help='Number of fake documents', default=50, type=click.INT, show_default=True)
+@click.option('--seed', help='Document generation seed', default=1234, type=click.INT, show_default=True)
+@click.option('--fast', is_flag=True, help='Only run fast detectors')
+@click.option('--locales', default=','.join(FILTH_IN_LOCALES.keys()), show_default=True,
+               metavar='<locale>', type=click.STRING, help='Locales to run with')
+def main(fast: bool, locales: Union[str, List[str]], ndocs: int = 50, seed: int = 1234):
+    """Test scrubadub accuracy using fake data."""
+    run_slow = not fast
+
+    if isinstance(locales, str):
+        locales_list = locales.split(',')
+    else:
+        locales_list = list(locales)
+
+    detector_available = load_complicated_detectors(run_slow)
+
+    settings = document_accuracy_settings(
+        locales=locales_list, run_slow=run_slow, detector_available=detector_available,
+    )
+
+    Faker.seed(seed)
+    random.seed(seed)
+
+    found_filth = []
+    for locale, filth_list, detectors in settings:
+        found_filth += generate_and_scrub(locale, filth_list, detectors, n_docs=ndocs)
+
+    print(get_filth_classification_report(found_filth))
+
+    # TODO: check seed sets things correctly
 
 
 if __name__ == "__main__":
