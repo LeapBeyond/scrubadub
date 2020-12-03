a2202e099dde9f4dc3038e6acf350bd98284940e
Thomas Bird <thomas.bird@leapbeyond.ai>
2020-12-03 12:10:51 +0000

    update internal package imports

--- scrubadub/filth/__init__.py
+++ scrubadub/filth/__init__.py
@@ -1,17 +1,21 @@
 from .base import Filth, MergedFilth, RegexFilth
 from .address import AddressFilth
 from .credential import CredentialFilth
+from .credit_card import CreditCardFilth
+from .drivers_licence import DriversLicenceFilth
 from .email import EmailFilth
 from .known import KnownFilth
+from .location import LocationFilth
 from .name import NameFilth
-from .named_entity import NamedEntityFilth
+# from .named_entity import NamedEntityFilth
 from .organization import OrganizationFilth
 from .phone import PhoneFilth
 from .postalcode import PostalCodeFilth
 from .skype import SkypeFilth
-from .ssn import SSNFilth
 from .twitter import TwitterFilth
 from .url import UrlFilth
-from .gb_nino import GBNinoFilth
-from .gb_drivers import GBDriversFilth
-from .gb_trn import GBTrnFilth
+from .vehicle_licence_plate import VehicleLicencePlateFilth
+
+from .en_GB.nino import NinoFilth
+from .en_GB.trn import TrnFilth
+from .en_US.ssn import SSNFilth
