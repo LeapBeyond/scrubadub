2853929f38be54a777e7b2f392a55b3127b0f5c2
Thomas Bird <thomas.bird@leapbeyond.ai>
2020-12-03 12:15:47 +0000

    add test for unknonwn filth type from spacy

--- tests/test_detector_spacy.py
+++ tests/test_detector_spacy.py
@@ -2,7 +2,7 @@ import sys
 import unittest
 
 from scrubadub import Scrubber
-from scrubadub.filth import NameFilth, OrganizationFilth, NamedEntityFilth
+from scrubadub.filth import NameFilth, OrganizationFilth, LocationFilth, Filth
 from base import BaseTestCase
 
 
@@ -58,18 +58,25 @@ class NamedEntityTestCase(unittest.TestCase, BaseTestCase):
     def test_organisations(self):
         doc_list = ["She started working for Apple this year",
                     "But used to work for Google"]
-        beg_end_list = [(24, 30),
+        beg_end_list = [(24, 29),
                         (21, 27)]
 
         self._assert_filth_type_and_pos(doc_list, beg_end_list, OrganizationFilth)
 
-    def test_other_entity(self):
+    def test_location(self):
         self.detector.named_entities = {"GPE"}
         doc_list = ["London is a city in England"]
         beg_end_list = [(0, 6),
                         (20, 27)]
 
-        self._assert_filth_type_and_pos(doc_list, beg_end_list, NamedEntityFilth)
+        self._assert_filth_type_and_pos(doc_list, beg_end_list, LocationFilth)
+
+    def test_unknown_entity(self):
+        self.detector.named_entities = {"PERCENT"}
+        doc_list = ["20% of the city is in ruins."]
+        beg_end_list = [(0, 3)]
+
+        self._assert_filth_type_and_pos(doc_list, beg_end_list, Filth)
 
     def test_wrong_model(self):
         """Test that it raises an error if user inputs invalid spacy model"""
