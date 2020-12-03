7a2b46457d78f5eacde94443fdceba456a88960e
Thomas Bird <thomas.bird@leapbeyond.ai>
2020-12-03 12:08:02 +0000

    add a viechicle licence plate detector

---
+++ scrubadub/filth/vehicle_licence_plate.py
@@ -0,0 +1,18 @@
+from faker import Faker
+
+from .base import Filth
+
+
+class VehicleLicencePlateFilth(Filth):
+    type = 'vehicle_licence_plate'
+
+    @staticmethod
+    def generate(faker: Faker) -> str:
+        """Generates an example of this ``Filth`` type, usually using the faker python library.
+
+        :param faker: The ``Faker`` class from the ``faker`` library
+        :type faker: Faker
+        :return: An example of this ``Filth``
+        :rtype: str
+        """
+        return faker.license_plate()
