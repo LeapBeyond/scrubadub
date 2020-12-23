f3f3111b52410f80fd60e5c21cef7443dba876bd
Thomas Bird <thomas.bird@leapbeyond.ai>
2020-12-23 19:20:39 +0000

    improve phone detector documentation

--- scrubadub/detectors/phone.py
+++ scrubadub/detectors/phone.py
@@ -8,18 +8,26 @@ from ..filth import PhoneFilth
 
 class PhoneDetector(Detector):
     """Remove phone numbers from dirty dirty ``text`` using
-    `python-phonenumbers
-    <https://github.com/daviddrysdale/python-phonenumbers>`_, a port of a
+    `python-phonenumbers <https://github.com/daviddrysdale/python-phonenumbers>`_, a port of a
     Google project to correctly format phone numbers in text.
 
-    ``region`` specifies the best guess region to start with (default:
-    ``"US"``). Specify ``None`` to only consider numbers with a leading
-    ``+`` to be considered.
+    Set the locale on the scrubber or detector to set the region used to search for valid phone numbers.
+    If the locale is set to 'en_CA' Canadian numbers will be searched for, while setting the local to 'en_GB' searches
+    for British numbers.
     """
     filth_cls = PhoneFilth
     name = 'phone'
 
     def iter_filth(self, text, document_name: Optional[str] = None):
+        """Yields discovered filth in the provided ``text``.
+
+        :param text: The dirty text to clean.
+        :type text: str
+        :param document_name: The name of the document to clean.
+        :type document_name: str, optional
+        :return: An iterator to the discovered :class:`Filth`
+        :rtype: Iterator[:class:`Filth`]
+        """
         # create a copy of text to handle multiple phone numbers correctly
         for match in phonenumbers.PhoneNumberMatcher(text, self.region):
             yield PhoneFilth(
