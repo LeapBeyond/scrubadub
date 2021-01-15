f181a0a37086dc47dbd0e1b8014af8eb35926e48
Thomas Bird <thomas.bird@leapbeyond.ai>
2021-01-15 16:45:26 +0000

    made the postcode regex more specific and also able to match a wider range of postcodes

--- scrubadub/detectors/postalcode.py
+++ scrubadub/detectors/postalcode.py
@@ -9,10 +9,13 @@ class PostalCodeDetector(RegionLocalisedRegexDetector):
     filth_cls = PostalCodeFilth
     name = 'postalcode'
     region_regex = {
+        # Informed by https://en.wikipedia.org/wiki/Postcodes_in_the_United_Kingdom#Validation
+        # and validated against https://osdatahub.os.uk/downloads/open/CodePointOpen
         'GB': re.compile(r"""
             (
+                # 
                 (?:[gG][iI][rR] {0,}0[aA]{2})|
-                (?:
+                (?:  # British Overseas Territories in usual format
                     (?:
                         [aA][sS][cC][nN]|
                         [sS][tT][hH][lL]|
@@ -26,13 +29,25 @@ class PostalCodeDetector(RegionLocalisedRegexDetector):
                     )
                     \ {0,}1[zZ]{2}
                 )|
-                (?:
+                (?:  # British Overseas Territories in zip-code format
+                    (KY[0-9]|MSR|VG|AI)[ -]{0,}[0-9]{4}
+                )|
+                # (?:  # Bermuda including this causes too many false positives, so excluded for now
+                #     [a-zA-Z]{2}\ {0,}[0-9]{2}
+                # )|
+                (?:  # British Forces Post Office
+                    [Bb][Ff][Pp][Oo]\ {0,}[0-9]{1,4}
+                )|
+                (?:  # Mainland British postcodes
                     (?:
-                        (?:[a-pr-uwyzA-PR-UWYZ][a-hk-yxA-HK-XY]?[0-9][0-9]?)|
-                        (?:
-                            (?:[a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|
-                            (?:[a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y])
-                        )
+                        (?:[Ww][Cc][0-9][abehmnprvwxyABEHMNPRVWXY])|
+                        (?:[Ee][Cc][1-4][abehmnprvwxyABEHMNPRVWXY])|
+                        (?:[Nn][Ww]1[Ww])|
+                        (?:[Ss][Ee]1[Pp])|
+                        (?:[Ss][Ww]1[abehmnprvwxyABEHMNPRVWXY])|
+                        (?:[EeNnWw]1[a-hjkpstuwA-HJKPSTUW])|
+                        (?:[BbEeGgLlMmNnSsWw][0-9][0-9]?)|
+                        (?:[a-pr-uwyzA-PR-UWYZ][a-hk-yxA-HK-XY][0-9][0-9]?)
                     )
                     \ {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}
                 )
