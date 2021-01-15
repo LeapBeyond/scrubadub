f33c50cabb7decebeefbdd7e6c1a100717c40cd6
Thomas Bird <thomas.bird@leapbeyond.ai>
2021-01-15 16:44:33 +0000

    add a test against a list of all post codes

--- tests/test_detector_postal_codes.py
+++ tests/test_detector_postal_codes.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import zipfile
+import pathlib
+import requests
 import unittest
 import warnings
 
@@ -49,3 +53,31 @@ class PostalCodesTestCase(unittest.TestCase):
                 self.assertEquals(matches[0].text, postal_code)
             else:
                 self.assertEquals(matches, [])
+
+    def test_extensive(self):
+        zip_location = pathlib.Path(__file__).parent / 'code_point_uk_post_codes.zip'
+
+        # Download an extensive list of all postcodes
+        if not zip_location.exists():
+            url = 'https://api.os.uk/downloads/v1/products/CodePointOpen/downloads?area=GB&format=CSV&redirect'
+            r = requests.get(url, allow_redirects=True)
+            with open(zip_location.name, 'wb') as f:
+                f.write(r.content)
+
+        detector = scrubadub.detectors.PostalCodeDetector(locale='en_GB')
+
+        # Run the detector against this list to ensure we pickup all post codes
+        with zipfile.ZipFile(zip_location.name) as zip:
+            data_file_names = [
+                name for name in zip.namelist()
+                if name.lower().endswith('.csv') and name.startswith('Data/CSV')
+            ]
+            for data_file_name in data_file_names:
+                with zip.open(data_file_name) as data_file:
+                    df = pd.read_csv(data_file, header=None)
+                    post_codes = df.loc[:, 0].sample(frac=.1).values.tolist()
+                    for post_code in post_codes:
+                        filth_list = list(detector.iter_filth(post_code))
+                        error_message = "Unable to match postcode {} from {}".format(post_code, data_file_name)
+                        self.assertEquals(1, len(filth_list), error_message)
+                        self.assertEquals(post_code, filth_list[0].text)
