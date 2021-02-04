600ee445c28f717052224c46fbc1e3e011c7abb5
Thomas Bird <thomas.bird@leapbeyond.ai>
2021-02-04 18:22:17 +0000

    partially update tests to new tokens

--- tests/test_detector_sklearn.py
+++ tests/test_detector_sklearn.py
@@ -25,58 +25,77 @@ class SklearnTestCase(unittest.TestCase):
         import scrubadub.detectors.sklearn
         det = scrubadub.detectors.sklearn.BIOTokenSklearnDetector()
 
+        from scrubadub.detectors.sklearn import TokenTupleWithLabel, TokenPosition
+
+        text = 'Card well protect drug professor now.\nInside present right both. Computer prepare style whose ' \
+               'business including big.'
+
         token_test_cases = [
+            # TODO: convert these tests cases to new format
+
+            # (
+            #     [(0, 5), (8, 10), (25, 26), ],
+            #     ["B-ADD", "I-ADD", "I-ADD", ],
+            #     {"minimum_ntokens": 1, "maximum_token_distance": 5, "b_token_required": False},
+            #     (
+            #         [(0, 10), (25, 26), ],
+            #         ["ADD", "ADD", ],
+            #     ),
+            # ),
+            # (
+            #     [(0, 5), (8, 10), (25, 26), ],
+            #     ["B-ADD", "I-ADD", "I-ADD", ],
+            #     {"minimum_ntokens": 1, "maximum_token_distance": 20, "b_token_required": False},
+            #     (
+            #         [(0, 26), ],
+            #         ["ADD", ],
+            #     ),
+            # ),
+            # (
+            #     [(0, 5), (8, 10), (25, 26), ],
+            #     ["B-ADD", "I-ADD", "I-ADD", ],
+            #     {"minimum_ntokens": 1, "maximum_token_distance": 4, "b_token_required": True},
+            #     (
+            #         [(0, 10), ],
+            #         ["ADD", ],
+            #     ),
+            # ),
+            # (
+            #     [(0, 5), (8, 10), (25, 26), ],
+            #     ["B-ADD", "I-ADD", "I-ADD", ],
+            #     {"minimum_ntokens": 2, "maximum_token_distance": 4, "b_token_required": False},
+            #     (
+            #         [(0, 10), ],
+            #         ["ADD", ],
+            #     ),
+            # ),
             (
-                [(0, 5), (8, 10), (25, 26), ],
-                ["B-ADD", "I-ADD", "I-ADD", ],
-                {"minimum_ntokens": 1, "maximum_token_distance": 5, "b_token_required": False},
-                (
-                    [(0, 10), (25, 26), ],
-                    ["ADD", "ADD", ],
-                ),
-            ),
-            (
-                [(0, 5), (8, 10), (25, 26), ],
-                ["B-ADD", "I-ADD", "I-ADD", ],
-                {"minimum_ntokens": 1, "maximum_token_distance": 20, "b_token_required": False},
-                (
-                    [(0, 26), ],
-                    ["ADD", ],
-                ),
-            ),
-            (
-                [(0, 5), (8, 10), (25, 26), ],
-                ["B-ADD", "I-ADD", "I-ADD", ],
-                {"minimum_ntokens": 1, "maximum_token_distance": 4, "b_token_required": True},
-                (
-                    [(0, 10), ],
-                    ["ADD", ],
-                ),
-            ),
-            (
-                [(0, 5), (8, 10), (25, 26), ],
-                ["B-ADD", "I-ADD", "I-ADD", ],
-                {"minimum_ntokens": 2, "maximum_token_distance": 4, "b_token_required": False},
-                (
-                    [(0, 10), ],
-                    ["ADD", ],
-                ),
-            ),
-            (
-                [(0, 5), (8, 10), (25, 26), (28, 36), (50, 55), (58, 64), (80, 88), ],
-                ["B-ADD", "I-ADD", "I-ADD", "I-ADD", "B-ADD", "I-ADD", "B-ADD", ],
-                {"minimum_ntokens": 2, "maximum_token_distance": 4, "b_token_required": True},
-                (
-                    [(0, 10), (50, 64), ],
-                    ["ADD", "ADD", ],
-                ),
+                [
+                    TokenTupleWithLabel('a.txt', text[0:5], TokenPosition(0, 5), 'B-ADD'),
+                    TokenTupleWithLabel('a.txt', text[8:10], TokenPosition(8, 10), 'I-ADD'),
+                    TokenTupleWithLabel('a.txt', text[25:26], TokenPosition(25, 26), 'I-ADD'),
+                    TokenTupleWithLabel('a.txt', text[28:36], TokenPosition(28, 36), 'I-ADD'),
+                    TokenTupleWithLabel('a.txt', text[50:55], TokenPosition(50, 55), 'B-ADD'),
+                    TokenTupleWithLabel('a.txt', text[58:64], TokenPosition(58, 64), 'I-ADD'),
+                    TokenTupleWithLabel('a.txt', text[80:88], TokenPosition(80, 88), 'I-ADD'),
+                ],
+                {
+                    "document_list": [text],
+                    "document_names": ['a.txt'],
+                    "minimum_ntokens": 2,
+                    "maximum_token_distance": 4,
+                    "b_token_required": True
+                },
+                [
+                    TokenTupleWithLabel('a.txt', text[0:10], TokenPosition(0, 10), 'ADD'),
+                    TokenTupleWithLabel('a.txt', text[50:64], TokenPosition(50, 64), 'ADD'),
+                ],
             ),
         ]
 
-        for text_token_positions, text_labels, kwargs, test_result in token_test_cases:
+        for tokens, kwargs, test_result in token_test_cases:
             result = det._combine_iob_tokens(
-                text_token_positions=text_token_positions,
-                text_labels=text_labels,
+                text_tokens=tokens,
                 **kwargs
             )
             self.assertEqual(test_result, result)
