89c343b861cb9dc5d796db97e3c016b4888a4c77
Thomas Bird <thomas.bird@leapbeyond.ai>
2021-03-08 15:14:04 +0000

    allow numeric replacement in the FilthTypeReplacer

--- scrubadub/post_processors/text_replacers/filth_type.py
+++ scrubadub/post_processors/text_replacers/filth_type.py
@@ -1,14 +1,26 @@
 from typing import Sequence, Optional
 
-from ...filth import Filth, MergedFilth
+from ...filth import Filth, MergedFilth, TaggedEvaluationFilth
 from ..base import PostProcessor
+from ... import utils
 
 
 class FilthTypeReplacer(PostProcessor):
     name = 'filth_type_replacer'  # type: str
 
+    # NOTE: this is not an efficient way to store this in memory. could
+    # alternatively hash the type and text and do away with the overhead
+    # bits of storing the tuple in the lookup
+    lookup = utils.Lookup()
+
+    def __init__(self, upper: bool = True, include_count: bool = False, separator: Optional[str] = None, **kawrgs):
+        super(FilthTypeReplacer, self).__init__(**kawrgs)
+        self.upper = upper
+        self.include_count = include_count
+        self.separator = separator
+
     @staticmethod
-    def filth_label(filth, upper: bool = True, separator: Optional[str] = None) -> str:
+    def filth_label(filth, upper: bool = True, include_count: bool = False, separator: Optional[str] = None) -> str:
         if separator is None:
             separator = '+'
         filths = [filth]
@@ -20,10 +32,19 @@ class FilthTypeReplacer(PostProcessor):
             filth_type = getattr(f, 'type', None)
             if filth_type is None:
                 continue
-            if filth_type == 'known':
+            if filth_type == TaggedEvaluationFilth.type:
                 filth_comparison_type = getattr(f, 'comparison_type', None)
                 if filth_comparison_type is not None:
                     filth_type += '_' + filth_comparison_type
+
+            filth_type = filth_type.replace(' ', '_')
+
+            if include_count:
+                filth_type = '{}-{:d}'.format(
+                    filth_type,
+                    FilthTypeReplacer.lookup[(filth_type, f.text.lower())]
+                )
+
             replacements.add(filth_type)
 
         # print(filths, replacements)
@@ -34,7 +55,12 @@ class FilthTypeReplacer(PostProcessor):
 
     def process_filth(self, filth_list: Sequence[Filth]) -> Sequence[Filth]:
         for filth_item in filth_list:
-            filth_item.replacement_string = self.filth_label(filth_item)
+            filth_item.replacement_string = self.filth_label(
+                filth=filth_item,
+                upper=self.upper,
+                include_count=self.include_count,
+                separator=self.separator,
+            )
         return filth_list
 
 
