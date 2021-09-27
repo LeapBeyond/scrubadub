46bd7e2b58003eda4dbf2a23ea126537a75dafff
Thomas Bird <thomas.bird@leapbeyond.ai>
2021-09-27 16:24:41 +0100

    update regex to include a word boundry and require a dot in th domain. enable the code to check if theres an at-sign and a dot in the document before trying the full regex

--- scrubadub/detectors/email.py
+++ scrubadub/detectors/email.py
@@ -23,32 +23,33 @@ class EmailDetector(RegexDetector):
     #
     # adapted from https://gist.github.com/dideler/5219706
     regex = re.compile((
-        r"[a-z0-9!#$%&'*+\/=?^_`{|}~-]"               # start with this character
+        r"\b[a-z0-9!#$%&'*+\/=?^_`{|}~-]"             # start with this character
         r"(?:"
         r"    [\.a-z0-9!#$%&'*+\/=?^_`{|}~-]{0,62}"   # valid next characters (max length 64 chars before @)
         r"    [a-z0-9!#$%&'*+\/=?^_`{|}~-]"           # end with this character
         r")?"
-        r"(@|\sat\s)"                                 # @ or the word 'at' instead
+        r"(?:@|\sat\s)"                               # @ or the word 'at' instead
         r"[a-z0-9]"                                   # domain starts like this
         r"(?:"
-        r"    (?:\.|\sdot\s|[a-z0-9-]){0,251}"        # might have any of these, with '.' or the word 'dot' instead
-        r"    [a-z0-9]"                               # max 253 chars, ends with one of these
-        r")?"
+        r"    (?=[a-z0-9-]*(\.|\sdot\s))"             # A lookahead to ensure there is a dot in the domain
+        r"    (?:\.|\sdot\s|[a-z0-9-]){0,251}"        # might have a '.' or the word 'dot' instead
+        r"    [a-z0-9]"                               # domain has max 253 chars, ends with one of these
+        r")+\b"
     ), re.VERBOSE | re.IGNORECASE)
 
-    at_matcher = re.compile(r"(?:@|\sat\s)", re.IGNORECASE)
-    dot_matcher = re.compile(r"(?:\.|\sdot\s)", re.IGNORECASE)
+    at_matcher = re.compile(r"@|\sat\s", re.IGNORECASE)
+    dot_matcher = re.compile(r"\.|\sdot\s", re.IGNORECASE)
 
-    # def iter_filth(self, text: str, document_name: Optional[str] = None) -> Generator[Filth, None, None]:
-    #     """Yields discovered filth in the provided ``text``.
-    #
-    #     :param text: The dirty text to clean.
-    #     :type text: str
-    #     :param document_name: The name of the document to clean.
-    #     :type document_name: str, optional
-    #     :return: An iterator to the discovered :class:`Filth`
-    #     :rtype: Iterator[:class:`Filth`]
-    #     """
-    #
-    #     if re.match(self.at_matcher, text) and re.match(self.dot_matcher, text):
-    #         yield from super().iter_filth(text=text, document_name=document_name)
+    def iter_filth(self, text: str, document_name: Optional[str] = None) -> Generator[Filth, None, None]:
+        """Yields discovered filth in the provided ``text``.
+
+        :param text: The dirty text to clean.
+        :type text: str
+        :param document_name: The name of the document to clean.
+        :type document_name: str, optional
+        :return: An iterator to the discovered :class:`Filth`
+        :rtype: Iterator[:class:`Filth`]
+        """
+
+        if re.search(self.at_matcher, text) and re.search(self.dot_matcher, text):
+            yield from super().iter_filth(text=text, document_name=document_name)
