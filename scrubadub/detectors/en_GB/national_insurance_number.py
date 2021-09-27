82a749319c5b3804c68ee62e79c87c70e696fadf
Thomas Bird <thomas.bird@leapbeyond.ai>
2021-09-27 16:26:24 +0100

    move to RegionLocalisedRegexDetector to reduce the code needed in detectors

--- scrubadub/detectors/en_GB/national_insurance_number.py
+++ scrubadub/detectors/en_GB/national_insurance_number.py
@@ -1,34 +1,22 @@
 import re
 
 from scrubadub.detectors.catalogue import register_detector
-from scrubadub.detectors.base import RegexDetector
+from scrubadub.detectors.base import RegionLocalisedRegexDetector
 from scrubadub.filth import NationalInsuranceNumberFilth
 
 
 @register_detector
-class NationalInsuranceNumberDetector(RegexDetector):
-    """Use regular expressions to remove the UK National Insurance number (NINO),
+class NationalInsuranceNumberDetector(RegionLocalisedRegexDetector):
+    """Use regular expressions to remove the GB National Insurance number (NINO),
     Simple pattern matching, no checksum solution.
     """
-
     name = 'national_insurance_number'
     autoload = True
     filth_cls = NationalInsuranceNumberFilth
     # this regex is looking for NINO that does not begin with certain letters
-    regex = re.compile(
-        r'(?!BG)(?!GB)(?!NK)(?!KN)(?!TN)(?!NT)(?!ZZ)(?:[A-CEGHJ-PR-TW-Z][A-CEGHJ-NPR-TW-Z])(?:\s*\d\s*){6}[A-D]',
-        re.IGNORECASE | re.VERBOSE
-    )
-
-    @classmethod
-    def supported_locale(cls, locale: str) -> bool:
-        """Returns true if this ``Detector`` supports the given locale.
-
-        :param locale: The locale of the documents in the format: 2 letter lower-case language code followed by an
-                       underscore and the two letter upper-case country code, eg "en_GB" or "de_CH".
-        :type locale: str
-        :return: ``True`` if the locale is supported, otherwise ``False``
-        :rtype: bool
-        """
-        language, region = cls.locale_split(locale)
-        return region in ['GB']
+    region_regex = {
+        'GB': re.compile(
+            r'(?!BG)(?!GB)(?!NK)(?!KN)(?!TN)(?!NT)(?!ZZ)(?:[A-CEGHJ-PR-TW-Z][A-CEGHJ-NPR-TW-Z])(?:\s*\d\s*){6}[A-D]',
+            re.IGNORECASE | re.VERBOSE
+        ),
+    }
--- scrubadub/detectors/en_GB/tax_reference_number.py
+++ scrubadub/detectors/en_GB/tax_reference_number.py
@@ -1,12 +1,12 @@
 import re
 
 from scrubadub.detectors.catalogue import register_detector
-from scrubadub.detectors.base import RegexDetector
+from scrubadub.detectors.base import RegionLocalisedRegexDetector
 from scrubadub.filth import TaxReferenceNumberFilth
 
 
 @register_detector
-class TaxReferenceNumberDetector(RegexDetector):
+class TaxReferenceNumberDetector(RegionLocalisedRegexDetector):
     """Use regular expressions to detect the UK PAYE temporary reference number (TRN),
     Simple pattern matching, no checksum solution.
     """
@@ -15,17 +15,6 @@ class TaxReferenceNumberDetector(RegexDetector):
     autoload = True
     filth_cls = TaxReferenceNumberFilth
     # this regex is looking for NINO that does not begin with certain letters
-    regex = re.compile(r'''\d{2}\s?[a-zA-Z]{1}(?:\s*\d\s*){5}''', re.IGNORECASE)
-
-    @classmethod
-    def supported_locale(cls, locale: str) -> bool:
-        """Returns true if this ``Detector`` supports the given locale.
-
-        :param locale: The locale of the documents in the format: 2 letter lower-case language code followed by an
-                       underscore and the two letter upper-case country code, eg "en_GB" or "de_CH".
-        :type locale: str
-        :return: ``True`` if the locale is supported, otherwise ``False``
-        :rtype: bool
-        """
-        language, region = cls.locale_split(locale)
-        return region in ['GB']
+    region_regex = {
+        'GB': re.compile(r'''\d{2}\s?[a-zA-Z]{1}(?:\s*\d\s*){5}''', re.IGNORECASE),
+    }
